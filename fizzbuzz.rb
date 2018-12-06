3d79e8c33898a7631b9185694513b5340f97d053
Daniel B Moore <danielmoorerealperson@gmail.com>
2018-12-06 20:22:52 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,20 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+def fizzbuzz(number)
+  if number % 3 == 0
+    puts "Fizz"
+
+  end
+
+  if number % 5 == 0
+    puts "Buzz"
+  end
+
+  if number % 3 == 0, number % 5 == 0
+    puts "FizzBuzz"
+  end
+
+elsif number % 3 or number % 5 > 0
+  
+
+
+
+end
